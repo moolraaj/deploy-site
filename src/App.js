ba967351ba0f738d47c088150cb7446c6abf06ac
moolraaj <116911577+moolraaj@users.noreply.github.com>
2024-01-24 19:15:44 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -9,12 +9,8 @@ function App() {
   return (
      <>
   <h1>hii sparkweb</h1>
-     <h1>this is react js creteated by rakesh kumar</h1>
-     <h2>Hello raaj sir kyaa haal hai </h2>
-     <h1>this is react js file</h1>
-     <h2>this is react js file</h2>
-     <h2>this is third heading</h2>
-     <h2>this is third heading</h2>
+ 
+     
      <button type='button'>submit</button>
      <New/>
  
