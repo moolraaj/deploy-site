32b001723b3d8c3bc1261e049b5b8d927f4a4cc2
moolraaj <116911577+moolraaj@users.noreply.github.com>
2024-01-24 17:01:34 +0530

    raaj-commit ot raaj branch

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import New from './New';
 function App() {
   return (
      <>
+     <h1>this is react js file</h1>
+     <h2>this is react js file</h2>
+     <h2>this is third heading</h2>
      <New/>
      </>
      
