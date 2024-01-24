a6a42071aeccdc38c04635d55bc74d9708826b27
Mr-rakesh-rk <rkrakesh24726@gmail.com>
2024-01-24 18:41:20 +0530

    added

--- src/App.js
+++ src/App.js
@@ -8,11 +8,7 @@ function App() {
 
   return (
      <>
-     <h1>hello  im raaj im full stack developer im also MERN stack developer</h1>
-     <h1>hello im react js developer since 6 years..</h1>
-     <h1>this is react js file</h1>
-     <h2>this is react js file</h2>
-     <h2>this is third heading</h2>
+  <h1>hii sparkweb</h1>
      <h1>this is react js creteated by rakesh kumar</h1>
      <h2>Hello raaj sir kyaa haal hai </h2>
      <button type='button'>submit</button>
