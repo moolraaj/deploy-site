d694743c420b364d683191a215d25b36e42a8e47
moolraaj <116911577+moolraaj@users.noreply.github.com>
2024-01-24 18:37:25 +0530

    remove api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import React, { useState, useEffect } from "react";
+ 
 import './App.css';
 import New from './New';
 
 function App() {
 
-
-  const [fakeStore, setFakeStore] = useState([]);
-
-  const fetchData = async () => {
-    try {
-      const response = await fetch('https://fakestoreapi.com/products');
-      const data = await response.json();
-      setFakeStore(data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
+ 
 
   return (
      <>
@@ -32,22 +17,7 @@ function App() {
      <h2>Hello raaj sir kyaa haal hai </h2>
      <button type='button'>submit</button>
      <New/>
-
-     <div className="Fake-Store">
-      <h1>Fake Store Products</h1>
-      <ul className="product-list">
-        {fakeStore.map(product => (
-          <li key={product.id} className="product-item">
-            <img src={product.image} alt="prodImg" className="product-image" />
-            <div className="product-details">
-              <h2>{product.title}</h2>
-              <p>{product.description}</p>
-              <p>Price: ${product.price}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
-      </div>
+ 
 
 
      
